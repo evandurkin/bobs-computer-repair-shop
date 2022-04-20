410656b916afd6f016e1bf2778e9a2fd935e8248
Gunner Bradley <gunnerbradley.dev@gmail.com>
2022-04-20 00:06:18 -0500

    added db config

--- server/app.js
+++ server/app.js
@@ -1,43 +1,46 @@
 /**
  * Require statements
  */
-const express = require('express');
-const http = require('http');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require("express");
+const http = require("http");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const path = require("path");
+const mongoose = require("mongoose");
 
 /**
  * App configurations
  */
 let app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({'extended': true}));
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../dist/bcrs')));
-app.use('/', express.static(path.join(__dirname, '../dist/bcrs')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../dist/bcrs")));
+app.use("/", express.static(path.join(__dirname, "../dist/bcrs")));
 
 /**
  * Variables
  */
-const port = 3000; // server port
+const port = process.env.port || 3000; // server port
 
-// TODO: This line will need to be replaced with your actual database connection string
-const conn = 'mongodb+srv://superadmin:s3cret@cluster0-lujih.mongodb.net/bcrs?retryWrites=true&w=majority';
+// Database connection string - mongoDB account configured to accept request from any IP
+const conn = `mongodb+srv://450-user:Qwerty123@cluster0.mlnw2.mongodb.net/bcrs?retryWrites=true&w=majority`;
 
 /**
  * Database connection
  */
-mongoose.connect(conn, {
-  promiseLibrary: require('bluebird'),
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}).then(() => {
-  console.debug(`Connection to the database instance was successful`);
-}).catch(err => {
-  console.log(`MongoDB Error: ${err.message}`)
-}); // end mongoose connection
+mongoose
+  .connect(conn, {
+    promiseLibrary: require("bluebird"),
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.debug(`Connection to the database instance was successful`);
+  })
+  .catch((err) => {
+    console.log(`MongoDB Error: ${err.message}`);
+  }); // end mongoose connection
 
 /**
  * API(s) go here...
@@ -46,6 +49,6 @@ mongoose.connect(conn, {
 /**
  * Create and start server
  */
-http.createServer(app).listen(port, function() {
-  console.log(`Application started and listening on port: ${port}`)
+http.createServer(app).listen(port, function () {
+  console.log(`Application started and listening on port: ${port}`);
 }); // end http create server function
