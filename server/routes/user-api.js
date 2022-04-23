593ce24a7bc28d1020cf2a62d9e2dc194a65f0ef
Evan Durkin <edurkin@my365.bellevue.edu>
2022-04-23 09:41:50 -0400

    adding saltRounds variable

--- server/routes/user-api.js
+++ server/routes/user-api.js
@@ -15,6 +15,7 @@ const BaseResponse = require("../services/base-response");
 const ErrorResponse = require("../services/error-response");
 
 let router = express.Router();
+const saltRounds = 10;
 
 // findAllUsers API
 router.get("/", async (req, res) => {
