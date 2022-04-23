77e7c98614009576b3839fb71e92eebf78a185d1
Evan Durkin <edurkin@my365.bellevue.edu>
2022-04-23 14:57:56 -0400

    adding security question edit component updates

--- src/app/pages/security-question-edit/security-question-edit.component.ts
+++ src/app/pages/security-question-edit/security-question-edit.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+/*
+=======================================
+// Title: Bob’s Computer Repair Shop
+// Date: 04 April 2022
+// Authors: Evan Durkin, Keith Hall,
+// Gustavo Roo Gonzalez, and Gunner Bradley
+// Description: updating user dialog
+=======================================
+*/
+
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SecurityQuestion } from 'src/app/shared/interfaces/security-question';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-security-question-edit',
@@ -7,9 +21,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecurityQuestionEditComponent implements OnInit {
 
-  constructor() { }
+  questionForm: FormGroup;
+  questionData: SecurityQuestion;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<SecurityQuestionEditComponent>,
+    @Inject(MAT_DIALOG_DATA) data
+  ) {
+    this.questionData = data.questionData
   }
 
+  ngOnInit(): void {
+    console.log(this.questionData.text)
+    this.questionForm = new FormGroup({
+      text: new FormControl(this.questionData.text, Validators.required)
+    })
+  }
+
+  editQuestion() {
+    this.dialogRef.close(this.questionForm.value);
+  }
+
+  cancel(){
+    this.dialogRef.close();
+  }
 }
