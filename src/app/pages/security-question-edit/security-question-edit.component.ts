2fb291d64d4fca042496e662efc70d135478e5b2
Evan Durkin <edurkin@my365.bellevue.edu>
2022-04-24 10:35:30 -0400

    adding security question ts updates

--- src/app/pages/security-question-edit/security-question-edit.component.ts
+++ src/app/pages/security-question-edit/security-question-edit.component.ts
@@ -8,11 +8,11 @@
 =======================================
 */
 
-import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
 import { SecurityQuestion } from 'src/app/shared/interfaces/security-question';
+import { SecurityQuestionService } from 'src/app/shared/services/security-question.service';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-security-question-edit',
@@ -23,27 +23,43 @@ export class SecurityQuestionEditComponent implements OnInit {
 
   questionForm: FormGroup;
   questionData: SecurityQuestion;
+  questionId: string;
 
   constructor(
+    private route: ActivatedRoute,
+    private router: Router,
     private fb: FormBuilder,
-    private dialogRef: MatDialogRef<SecurityQuestionEditComponent>,
-    @Inject(MAT_DIALOG_DATA) data
-  ) {
-    this.questionData = data.questionData
-  }
+    private securityQuestionService: SecurityQuestionService)
+    {
+    this.questionId = this.route.snapshot.paramMap.get('questionId');
 
-  ngOnInit(): void {
-    console.log(this.questionData.text)
-    this.questionForm = new FormGroup({
-      text: new FormControl(this.questionData.text, Validators.required)
+    this.securityQuestionService.findSecurityQuestionById(this.questionId).subscribe(res => {
+      this.questionData = res['data'];
+    }, err => {
+      console.log(err);
+    }, () => {
+      this.questionForm.controls.text.setValue(this.questionData.text);
     })
   }
 
-  editQuestion() {
-    this.dialogRef.close(this.questionForm.value);
+  ngOnInit(): void {
+    this.questionForm = this.fb.group({
+      text: [null, Validators.compose([Validators.required])],
+    });
   }
 
-  cancel(){
-    this.dialogRef.close();
+  editQuestion(): void {
+    const updatedSecurityQuestion: SecurityQuestion = {
+      text: this.questionForm.controls.text.value,
+      _id: ''
+    }
+
+    this.securityQuestionService.updateSecurityQuestion(this.questionId, updatedSecurityQuestion).subscribe(res => {
+      this.router.navigate(['/security-questions'])
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/security-questions'])
   }
 }
