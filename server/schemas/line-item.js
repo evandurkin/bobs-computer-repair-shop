9def4c1195487bfa4733e271f4b7018b255b7d1b
Evan Durkin <edurkin@my365.bellevue.edu>
2022-05-03 07:54:34 -0400

    adding line item schema

--- server/schemas/line-item.js
+++ server/schemas/line-item.js
@@ -1,7 +1,7 @@
 /*
 =======================================
 // Title: Bob’s Computer Repair Shop
-// Date: 22 April 2022
+// Date: 3 May 2022
 // Authors: Evan Durkin, Keith Hall,
 // Gustavo Roo Gonzalez, and Gunner Bradley
 // Description: Line item schema for BCRS.
