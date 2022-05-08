dfbc5277b8c9ad2e5ccba6dfe6127e3da09f764c
goostavo25 <groogonzalez@my365.bellevue.edu>
2022-05-07 21:55:14 -0400

    Fix Edit Role

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -98,7 +98,7 @@ const routes: Routes = [
         component: RoleCreateComponent,
       },
       {
-        path: 'role-details',
+        path: 'roles/:roleId',
         component: RoleDetailsComponent,
       },
       {
