776a5334409408b9e54eb190c3e8e09fec0c926e
Keith Hall <hkeith62@gmail.com>
2022-05-17 09:56:31 -0500

    Completed profile page

--- src/app/pages/edit-profile-password/edit-profile-password.component.spec.ts
+++ src/app/pages/edit-profile-password/edit-profile-password.component.spec.ts
@@ -1,3 +1,13 @@
+/*
+============================================
+// Title: Bob’s Computer Repair Shop
+// Date: 16 May 2022
+// Authors: Evan Durkin, Keith Hall,
+// Gustavo Roo Gonzalez, and Gunner Bradley
+// Description: Test file for the edit-profile-profile component.
+============================================
+*/
+
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditProfilePasswordComponent } from './edit-profile-password.component';
--- src/app/pages/edit-profile-password/edit-profile-password.component.ts
+++ src/app/pages/edit-profile-password/edit-profile-password.component.ts
@@ -1,4 +1,18 @@
+/*
+============================================
+// Title: Bob’s Computer Repair Shop
+// Date: 16 May 2022
+// Authors: Evan Durkin, Keith Hall,
+// Gustavo Roo Gonzalez, and Gunner Bradley
+// Description: TS file for the edit-profile-password component.
+============================================
+*/
+
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-edit-profile-password',
@@ -7,9 +21,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfilePasswordComponent implements OnInit {
 
-  constructor() { }
+  isAuthenticated: string;
+  username: string;
+  form: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router, private fb: FormBuilder, private cookieService: CookieService) {
+    this.isAuthenticated = this.route.snapshot.queryParamMap.get('isAuthenticated');
+    this.username = this.route.snapshot.queryParamMap.get('username');
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      password: [null, Validators.compose([Validators.required, Validators.pattern('^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$')])]
+    });
+  }
+  resetPassword() {
+    this.http.post('/api/session/users/' + this.username + '/reset-password', {
+      password: this.form.controls['password'].value
+    }).subscribe(res => {
+
+      this.cookieService.set('sessionuser', this.username, 1);
+      this.router.navigate(['/post-session/sign-in']);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
